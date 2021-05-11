bd4f3bc79ff372eb300f68af3a9b465d6ba75d9b
Hiro-1978 <hiromiuki11@gmail.com>
2021-05-11 10:30:57 +0700

    JavaOOP
    setName

---
+++ src/oop2/Human.java
@@ -0,0 +1,23 @@
+
+package oop2;
+
+/**
+ *
+ * @author Hiro-11
+ */
+public class Human {
+    
+    private String name = "Havana";
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        System.out.println("Hello "+name);
+    }
+    /*public void Hello(){
+        System.out.println("Hello "+name);
+    }
+*/
+}
---
+++ src/oop2/OOP2.java
@@ -0,0 +1,14 @@
+
+package oop2;
+
+/**
+ *
+ * @author Hiro-11
+ */
+public class OOP2 {
+    public static void main(String[] args){
+        Human h1 = new Human();
+        System.out.println("Hello "+h1.getName());
+        h1.setName("Paisan Pompadech");
+    }   
+}
